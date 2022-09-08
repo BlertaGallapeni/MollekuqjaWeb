15394271ee9107c7a3bbcbbd018b02adfa841d99
Blerta Gallapeni <76885867+BlertaGallapeni@users.noreply.github.com>
2022-09-09 00:44:06 +0200

    added the files of web

---
+++ js/script.js
@@ -0,0 +1,41 @@
+AOS.init({
+    duration: 800,
+    offset: 150,
+});
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle("scrolling-active", windowPosition);
+});
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
